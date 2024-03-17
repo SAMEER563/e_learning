f91389c35baccbcbd5e8389af04d73c40d7a1601
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-17 18:13:36 +0530

    Patch API & Course description form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
 import { LayoutDashboard } from "lucide-react";
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form.tsx";
 
 
 const CourseIdPage = async ({
@@ -61,6 +63,14 @@ const CourseIdPage = async ({
                      Customize your course
                    </h2>
                 </div>
+                <TitleForm 
+                 initialData={course}
+                 courseId={course.id}
+                />
+                 <DescriptionForm 
+                 initialData={course}
+                 courseId={course.id}
+                />
             </div>
           </div>
         </div>
