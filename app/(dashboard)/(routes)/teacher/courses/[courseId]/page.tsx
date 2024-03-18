84aef0938a1b5b2653f76432347b955e67f319fe
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-18 15:51:43 +0530

    Course price form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,11 +2,12 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
-import { LayoutDashboard } from "lucide-react";
+import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form.tsx";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
+import { PriceForm } from "./_components/price-form";
 
 
 const CourseIdPage = async ({
@@ -94,6 +95,34 @@ const CourseIdPage = async ({
                     value: category.id,
                  }))}
                 />
+            </div>
+            <div className="space-y-6">
+               <div>
+                 <div className="flex items-center gap-x-2">
+                   <IconBadge icon={ListChecks}/>
+                   <h2 className="text-xl">
+                    Course Chapters
+                   </h2>
+                 </div>
+                 <div>
+                   TODO: Chapters 
+                 </div>
+               </div>
+               <div>
+                <div className="flex items-center gap-x-2">
+                   <IconBadge icon={CircleDollarSign}/>
+                   <h2 className="text-xl">
+                    Sell your course
+                   </h2>
+                </div>
+                <PriceForm 
+                  initialData={course}
+                  courseId={course.id}
+                />
+               </div>
+            </div>
+            <div>
+                
             </div>
           </div>
         </div>
---
+++ lib/format.ts
@@ -0,0 +1,6 @@
+export const formatePrice = (price: number) => {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "INR"
+    }).format(price)
+}
