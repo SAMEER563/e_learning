e145c7f898b7155255a0e41344728c67610b92f6
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-20 01:49:51 +0530

    Chapter description form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -7,6 +7,7 @@ import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
 
 import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 
 
 const ChapterIdPage = async ({
@@ -83,6 +84,11 @@ const ChapterIdPage = async ({
                 </h2>
               </div>
               <ChapterTitleForm
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
+                <ChapterDescriptionForm
                 initialData={chapter}
                 courseId={params.courseId}
                 chapterId={params.chapterId}
